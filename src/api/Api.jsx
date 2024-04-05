2d7c214a95d56b8cfa5c27785c19f6cb6ada2b80
Rafael Freitas <rafaafreitasf@hotmail.com>
2024-04-05 18:32:47 -0300

    Finalizando teste de api

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import UserPost from './endpoints/UserPost';
 import TokenPost from './endpoints/TokenPost';
+import PhotoPost from './endpoints/PhotoPost';
+import PhotoGet from './endpoints/PhotoGet';
 
 function Api() {
   return (
     <div>
-      <h1>User Post</h1>
+      <h2>User Post</h2>
       <UserPost></UserPost>
-      <h1>Token Request</h1>
+      <h2>Token Request</h2>
       <TokenPost></TokenPost>
+      <h2>PHOTO POST</h2>
+      <PhotoPost></PhotoPost>
+      <h2>PHOTO GET</h2>
+      <PhotoGet></PhotoGet>
     </div>
   );
 }
