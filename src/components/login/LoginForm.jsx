29239d0885aa997d49ae68cae3d24708ebdadd92
Rafael Freitas <rafaafreitasf@hotmail.com>
2024-04-12 19:49:57 -0300

    useForm

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,39 +2,52 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../forms/Input';
 import Button from '../forms/Button';
+import useForm from '../../hooks/useForm';
 
 function LoginForm() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+    if (username.isValid() && password.isValid()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
     <div>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username"></Input>
-        <Input label="Senha" type="password" name="password"></Input>
+        <Input
+          label="Usuário"
+          type="text"
+          name="username"
+          {...username}
+        ></Input>
+        <Input
+          label="Senha"
+          type="password"
+          name="password"
+          {...password}
+        ></Input>
 
         <Button disabled>Entrar</Button>
       </form>
